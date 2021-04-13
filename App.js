c685f63bc2cea05ce6bf8aac3b0fb547cdeebdc4
j-ij-i <sunmlee126@gmail.com>
2021-04-13 18:14:03 +0900

    main image

--- App.js
+++ App.js
@@ -1,22 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./Loading";
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>hi!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Loading />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    color: "black",
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
